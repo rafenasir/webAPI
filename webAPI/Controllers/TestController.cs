209ef33466ce727204676eaa19ffd9bb5848fc07
Rafe Nasir <70711865+rafenasir@users.noreply.github.com>
2022-12-04 00:43:39 +0100

    test route

---
+++ webAPI/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+          
+            return "Test Result";
+        }
+    }
+}
